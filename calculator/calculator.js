003e17195219d6f9e81d2f3753d3f44193acf7ef
Mandi Haase <haase1020@gmail.com>
2020-03-25 12:32:38 -0400

    update notes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,3 +5,16 @@ module.exports = {
 function add() {
   return null;
 }
+
+
+//function add(a,b) {
+  //return a + b;
+// --> an example of a pure function --> doesn't rely on any outside functions
+//}
+
+
+//when testing, start with low hanging fruit --> happy path (your basic assumptions)
+//check for invalid input
+//then move on to edge cases
+//regression: code that WAS working stops working
+//unit testing is more GRANULAR --> 
